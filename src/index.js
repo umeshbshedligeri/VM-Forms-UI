561b7884a5e891ea6c09085f00f84f5edc25f0fc
umeshbshedligeri <umesh.s@vmokshagroup.com>
2021-03-09 12:38:14 +0530

    initial commit on vm forms ui ,integrated bootstrap template in react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+// ReactDOM.render(
+//   // <React.StrictMode>
+//     <App />
+//   // </React.StrictMode>,
+//   ,
+//   document.getElementById('root')
+// );
+render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
